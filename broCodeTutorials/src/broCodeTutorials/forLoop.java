ea6847d5bc28a7c0406175de24e6a67aac5c2efa
vagdevik <vagdevi.k15@gmail.com>
2021-09-05 21:02:00 -0500

    for loop in Java

---
+++ broCodeTutorials/src/broCodeTutorials/forLoop.java
@@ -0,0 +1,15 @@
+package broCodeTutorials;
+
+public class forLoop {
+
+	public static void main(String[] args) {
+		// for loop = executes a block of code a limited amount of times
+		
+				for(int i=10; i>=0; i--) {
+					System.out.println(i);
+				}
+				System.out.println("Happy new year!");
+
+	}
+
+}
