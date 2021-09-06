dde10b9bccf1ebbfd436b927232c1d4e1026ad31
vagdevik <vagdevi.k15@gmail.com>
2021-09-05 20:59:09 -0500

    while in Java

---
+++ broCodeTutorials/src/broCodeTutorials/whileJava.java
@@ -0,0 +1,20 @@
+package broCodeTutorials;
+import java.util.Scanner;
+
+public class whileJava {
+
+	public static void main(String[] args) {
+		// while loop = executes a block of code as long as a it's condition remains true
+
+				Scanner scanner = new Scanner(System.in);
+				String name = "";
+				
+				while(name.isBlank()) {
+					System.out.print("Enter your name: ");
+					name = scanner.nextLine();
+				}
+				System.out.println("Welcome "+name);
+
+	}
+
+}
